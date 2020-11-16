3fd6b9c33933f2be59216b2422d0484eb412584b
ridafarrukh <ridafarrukh92@gmail.com>
2020-11-16 21:18:45 +0100

    create class mobiltest

--- src/Mobiltest.java
+++ src/Mobiltest.java
@@ -1,10 +1,13 @@
 /**
  * created by Rida Warsi
  * Date : 2020-11-16
- * Time : 21:13
+ * Time : 21:17
  * Project : Metoderövning
  * Copyright : MIT
  */
 
-public class Mobil {
+public class Mobiltest {
+    public static void main(String[] args) {
+
+    }
 }
